8c06e9b0baa17c3dc56f60fd814f0a24bef4fbe0
ceelal <147487841+ceelal@users.noreply.github.com>
2023-10-10 11:26:38 +0300

    100%

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cecayir <cecayir@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 03:55:31 by cecayir           #+#    #+#             */
+/*   Updated: 2023/02/14 03:55:32 by cecayir          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i1;
+	unsigned int	i2;
+
+	i1 = 0;
+	i2 = 0;
+	while (dest[i1] != '\0')
+		i1++;
+	while (src[i2] != '\0' && i2 < nb)
+	{
+		dest[i1] = src[i2];
+		i1++;
+		i2++;
+	}
+	dest[i1] = '\0';
+	return (dest);
+}
---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cecayir <cecayir@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 00:52:46 by cecayir           #+#    #+#             */
+/*   Updated: 2023/02/16 01:29:09 by cecayir          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	dlen;
+	unsigned int	slen;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	dlen = j;
+	slen = ft_strlen(src);
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src [i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dlen + slen);
+}
