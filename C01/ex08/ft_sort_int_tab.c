f9455a2aa566aaea1fd487268ef04c2c0a5d913f
ceelal <147487841+ceelal@users.noreply.github.com>
2023-10-10 11:26:01 +0300

    100%

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cecayir <cecayir@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 10:14:12 by cecayir           #+#    #+#             */
+/*   Updated: 2023/02/11 10:14:18 by cecayir          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		swap;
+	int		count;
+
+	count = 0;
+	while (count < (size - 1))
+	{
+		if (tab[count] > tab[count + 1])
+		{
+			swap = tab[count];
+			tab[count] = tab[count + 1];
+			tab[count + 1] = swap;
+			count = 0;
+		}
+		else
+			count++;
+	}
+}
